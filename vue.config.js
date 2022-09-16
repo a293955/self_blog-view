81b3250fc677b4991f2b91d1e7421eeb9ecb1cf9
a293955 <xs2363354412@qq.com>
2022-09-16 13:23:07 +0800

    代码优化

--- vue.config.js
+++ vue.config.js
@@ -17,11 +17,7 @@ module.exports = {
 			//入口起点的最大体积
 			maxEntrypointSize: 50000000,
 			//生成文件的最大体积
-			maxAssetSize: 30000000,
-			//只给出 js 文件的性能提示
-			assetFilter: function(assetFilename) {
-				return assetFilename.endsWith('.js');
-			}
+			maxAssetSize: 30000000
 		}
 	}
 }
